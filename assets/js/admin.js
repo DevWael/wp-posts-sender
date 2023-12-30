8f453da16eb1defb469b1035c703a3882c30ee3f
devwael <dev.ahmedwael@gmail.com>
2023-12-30 03:05:50 +0200

    Display error message

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,9 +4,9 @@
     $(document).on('click', '.wp-posts-sender-button', function (e) {
         e.preventDefault();
         let button = $(this),
-            siteUrl = $(this).data('site-url'),
-            nonce = $(this).data('nonce'),
-            postId = $(this).data('post-id');
+            siteUrl = button.data('site-url'),
+            nonce = button.data('nonce'),
+            postId = button.data('post-id');
 
         if (button.hasClass('disabled')) {
             return;
@@ -24,6 +24,7 @@
             // display loading indicator
             beforeSend: function () {
                 button.find('span').remove();
+                button.parent().find('p').remove();
                 button.append('<span class="dashicons dashicons-update" style="margin-top: 4px"></span>');
             },
             success: function (response) {
@@ -33,6 +34,11 @@
                     button.find('span').remove();
                     button.append('<span class="dashicons dashicons-yes" style="margin-top: 4px"></span>');
                     button.addClass('disabled');
+                } else {
+                    button.find('span').remove();
+                    button.parent().find('p').remove();
+                    button.append('<span class="dashicons dashicons-no" style="margin-top: 4px"></span>');
+                    button.parent().append('<p class="wp-posts-sender-error" style="color: red">' + response.data.message + '</p>');
                 }
             }
         });
